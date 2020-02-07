1e4ae8f7d9461daa9d1582362b78284a22f4f46f
hus0124 <57971749+hus0124@users.noreply.github.com>
2020-02-07 13:51:21 +0900

    p_source

---
+++ P_source/mcpTest.py
@@ -0,0 +1,17 @@
+import spidev, time
+import RPi.GPIO as GPIO
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=1350000
+
+def analog_read(channel):
+    r = spi.xfer2([1, (8+channel) << 4,0])
+    adc_out = ((r[1]&3)<<8)+r[2]
+    return adc_out
+
+while True :
+    reading = analog_read(1)
+    voltage = reading * 3.3/1024
+    print("Reading=%d\tVoltage=%f"%(reading, voltage))
+    time.sleep(1)
